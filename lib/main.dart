67026cb352cec36ea929fc88a4d4830f2d8d2ddf
Souvik Biswas <sbis1999@gmail.com>
2020-01-09 11:52:19 +0530

    Code refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,10 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Icon(
+          Icons.favorite,
+          size: 30,
+        ),
       ),
     );
   }
